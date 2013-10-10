92bd5ed2a47b39686efab7aa6ddfedefae1030b9
Eli Dupree <vcs@elidupree.com>
2013-10-09 23:48:28 -0400

    added user-defined substitutions, fixed (stuff)(stuff) behavior in arithmetic

--- bot.py
+++ bot.py
@@ -19,9 +19,9 @@ send("NICK "+nick+"\r\n")
 
 def eval_arithmetic(string):
   def eval_match(match):
-    return str(eval_arithmetic(match.group(0)[1:-1]))
+    return " "+str(eval_arithmetic(match.group(1)))+" "
   while True:
-    parens_out = re.sub(r"\([^\(\)]*\)", eval_match, string)
+    parens_out = re.sub(r"\(([^\(\)]*)\)", eval_match, string)
     if parens_out == string:
       string = parens_out
       break
@@ -30,10 +30,10 @@ def eval_arithmetic(string):
   while True:
     def mult_fix(match):
       try:
-        return str((int(match.group(1)) * int(match.group(3))) if (match.group(2) == "*") else (int(match.group(1)) // int(match.group(3))))
+        return str((int(match.group(1)) // int(match.group(3))) if (match.group(2) == "/") else (int(match.group(1)) * int(match.group(3))))
       except ZeroDivisionError:
         raise Exception("eval_arithmetic failed with string: "+string)
-    mult_out = re.sub(r"(-?\d+)\s*([/\*])\s*(-?\d+)", mult_fix, string)
+    mult_out = re.sub(r"(-?\d+)\s*([/\*\s])\s*(-?\d+)", mult_fix, string)
     if mult_out == string:
       string = mult_out
       break
@@ -53,7 +53,8 @@ def eval_arithmetic(string):
   except ValueError:
     raise Exception("eval_arithmetic failed with string: "+string)
   
-
+subs = {}
+  
 while True:
   data = irc.recv(4096)
   print("Received: "+data)
@@ -73,32 +74,58 @@ while True:
     inited = True
   if (cmd == "PRIVMSG") and (info[2] == "#xkcd-qrpg") and (msg[0] == "!"):
     bot_command = msg[1:].strip()
-    def roll_repl(match):
-      dice = (1 if (match.group(1) == "") else int(match.group(1)))
-      sides = int(match.group(2))
-      if (dice < 1):
-        return "0"
-      if (sides < 1):
-        return "0"
-      if (dice > 30):
-        return "(more than 30 dice is too many)"
-      if (sides > 100000):
-        return "(more than 100000 sides is too many)"
-      return "("+"+".join([str(rand.randrange(1,sides+1)) for x in range(dice)])+")"
-    output = [bot_command]
-    roll = re.sub(r"(\d*)[Dd](\d+)", roll_repl, bot_command)
-    if roll != bot_command:
-      output.append(roll)
-    try:
-      output.append(str(eval_arithmetic(roll)))
-    except Exception as e:
-      if (str(e).find("eval_arithmetic failed") != -1):
-        print(str(e))
-      else:
-        raise e
-    else:
-      output[len(output)-1] = chr(2)+output[len(output)-1]+chr(2)
     
-    if (len(output) > 1):
-      send("PRIVMSG #xkcd-qrpg "+user+": "+" = ".join(output)+"\r\n")
+    def_match = re.match(r"def\s+([^\s]+)\s+(.*)", bot_command)
+    undef_match = re.match(r"undef\s+([^\s]+)", bot_command)
+    if def_match:
+      if user not in subs:
+        subs[user] = {}
+      subs[user][def_match.group(1)] = def_match.group(2)
+      send("PRIVMSG #xkcd-qrpg "+user+": defined '"+def_match.group(1)+"' as '"+def_match.group(2)+"'\r\n")
+    elif undef_match:
+      if user in subs:
+        del subs[user][undef_match.group(1)]
+      send("PRIVMSG #xkcd-qrpg "+user+": undefined '"+undef_match.group(1)+"'\r\n")
+    else:
+      output = [bot_command]
+    
+      subbed = bot_command
+      if user in subs:
+        for key in subs[user]:
+          subbed = subbed.replace(key, subs[user][key])
+      
+      if subbed != bot_command:
+        output.append(subbed)
+      
+      def roll_repl(match):
+        dice = (1 if (match.group(1) == "") else int(match.group(1)))
+        sides = int(match.group(2))
+        if (dice < 1):
+          return "0"
+        if (sides < 1):
+          return "0"
+        if (dice > 30):
+          return "(more than 30 dice is too many)"
+        if (sides > 100000):
+          return "(more than 100000 sides is too many)"
+        return "("+"+".join([str(rand.randrange(1,sides+1)) for x in range(dice)])+")"
+      
+      rolled = re.sub(r"(\d*)[Dd](\d+)", roll_repl, subbed)
+      if rolled != subbed:
+        output.append(rolled)
+    
+      try:
+        arithmeticked = str(eval_arithmetic(rolled))
+        if arithmeticked != rolled:
+          output.append(arithmeticked)
+      except Exception as e:
+        if (str(e).find("eval_arithmetic failed") != -1):
+          print(str(e))
+        else:
+          raise e
+      else:
+        output[len(output)-1] = chr(2)+output[len(output)-1]+chr(2)
+      
+      if (len(output) > 1):
+        send("PRIVMSG #xkcd-qrpg "+user+": "+" = ".join(output)+"\r\n")
     
